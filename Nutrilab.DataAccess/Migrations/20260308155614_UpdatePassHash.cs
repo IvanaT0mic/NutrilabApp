5f8c98ecd1d2bd7b3e6550f22f56f8b88430ce0d
ivana.tomic <ivatomic98@gmail.com>
2026-03-08 16:57:57 +0100

    fixed password hash seed

---
+++ Nutrilab.DataAccess/Migrations/20260308155614_UpdatePassHash.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nutrilab.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePassHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "PasswordHash",
+                value: "$2a$11$t58KV3nhGBV.iCCVlDo7cekNrrlpziyB5YX8GE5ZxBZg1yC3PvB8m");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "PasswordHash",
+                value: "$2a$11$t58KV3nhGBV.iCCVlDo7cekNrrlpziyB5YX8GE5ZxBZg1yC3PvB8m");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "PasswordHash",
+                value: "$2a$11$E67GgfyxuXJa4s1WwWMYb..4dqZco51JsjqToW/RPjLBD/0CzZcIS");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "PasswordHash",
+                value: "$2a$11$r76Xawm/dDZHs8Y4nVjb4ujEtfy4cN9Unw7oF/xuo/26Q9N5qRL52");
+        }
+    }
+}
--- Nutrilab.DataAccess/Models/Users/Seed.cs
+++ Nutrilab.DataAccess/Models/Users/Seed.cs
@@ -10,25 +10,25 @@
             {
                 Id = 1,
                 Email = "admin@nutriplan.com",
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin123!"),
+                PasswordHash = "$2a$11$3FxdA3t4oyB3FO.CNmY.Tem4sy3AI.//yOaJO/suHNahlRyysGE3a",
                 CreatedAt = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
             },
             new User {
                 Id = 2,
                 Email = "maintainer@nutriplan.com",
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Test!123"),
+                PasswordHash = "$2a$11$t58KV3nhGBV.iCCVlDo7cekNrrlpziyB5YX8GE5ZxBZg1yC3PvB8m",
                 CreatedAt = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
             },
             new User {
                 Id = 3,
                 Email = "editor@nutriplan.com",
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Test!123"),
+                PasswordHash = "$2a$11$t58KV3nhGBV.iCCVlDo7cekNrrlpziyB5YX8GE5ZxBZg1yC3PvB8m",
                 CreatedAt = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
             },
             new User {
                 Id = 4,
                 Email = "user@nutriplan.com",
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Test!123"),
+                PasswordHash = "$2a$11$t58KV3nhGBV.iCCVlDo7cekNrrlpziyB5YX8GE5ZxBZg1yC3PvB8m",
                 CreatedAt = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc)
             }
         };
